8c719405e50357148434e4122354c21bc7e8d990
mheilner <mark.heilner4@gmail.com>
2023-09-12 09:39:28 -0600

    Adding Inode

---
+++ src/spell/SpellCorrector.java
@@ -0,0 +1,16 @@
+package spell;
+
+import java.io.IOException;
+
+public class SpellCorrector implements ISpellCorrector {
+
+    @Override
+    public void useDictionary(String dictionaryFileName) throws IOException {
+
+    }
+
+    @Override
+    public String suggestSimilarWord(String inputWord) {
+        return null;
+    }
+}
---
+++ src/spell/Trie.java
@@ -0,0 +1,26 @@
+package spell;
+
+public class Trie implements ITrie{
+    @Override
+    public void add(String word) {
+
+    }
+
+    @Override
+    public INode find(String word) {
+        return null;
+    }
+
+    @Override
+    public int getWordCount() {
+        return 0;
+    }
+
+    @Override
+    public int getNodeCount() {
+        return 0;
+    }
+
+//    Will have to add in the other interface objects in the interface (reason it doesnt pull is that it is from object class
+
+}

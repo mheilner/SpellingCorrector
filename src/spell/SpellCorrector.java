fde47473c9b99c0ae1beb850dab50998288cd834
mheilner <mark.heilner4@gmail.com>
2023-09-18 20:32:02 -0600

    Got the usedictionary function working based on the test 1 of the function

--- src/spell/SpellCorrector.java
+++ src/spell/SpellCorrector.java
@@ -1,16 +1,41 @@
 package spell;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class SpellCorrector implements ISpellCorrector {
 
+    //Instance Variables
+    private Trie dictTrie;
+
+    //Constructor
+    public SpellCorrector(){
+        dictTrie = new Trie();
+    }
+
     @Override
     public void useDictionary(String dictionaryFileName) throws IOException {
+        BufferedReader read = new BufferedReader(new FileReader((dictionaryFileName)));
+        String line;
+        while ((line = read.readLine()) != null){
+            //Store all the words as lowercase
+            dictTrie.add(line.toLowerCase());
+        }
+        read.close();
 
     }
 
     @Override
     public String suggestSimilarWord(String inputWord) {
+        //Make the word lowercase
+        inputWord = inputWord.toLowerCase();
+
+        //Check if the word is the same
+        if (dictTrie.find(inputWord)!=null){
+            return inputWord;
+        }
+
         return null;
     }
 }

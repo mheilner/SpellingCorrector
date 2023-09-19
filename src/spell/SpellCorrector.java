1b514ae9bfc1b6ca95f666c3be345c404d312a86
mheilner <mark.heilner4@gmail.com>
2023-09-19 11:22:15 -0600

    finished project

--- src/spell/SpellCorrector.java
+++ src/spell/SpellCorrector.java
@@ -4,7 +4,11 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 //My imports
+import java.io.PrintStream;
+import java.sql.SQLOutput;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 
@@ -26,9 +30,6 @@ public class SpellCorrector implements ISpellCorrector {
             String[] words = line.toLowerCase().split("\\s+"); // Split by one or more whitespace characters
             for (String word : words) {
                 if (!word.isEmpty()) {
-//                    System.out.println(word);
-//                    System.out.println(word.length());
-                    // Store the word as lowercase
                     dictTrie.add(word);
                 }
             }
@@ -42,81 +43,181 @@ public class SpellCorrector implements ISpellCorrector {
         inputWord = inputWord.toLowerCase();
 
         //Create HashMap of words
-        HashMap<String, Integer> dictSuggest = new HashMap<>();
+        HashMap<String, Integer> editDistOneDict = new HashMap<>();
+        HashMap<String, Integer> editDistTwoDict = new HashMap<>();
 
         //Check if the word is the same
         if (dictTrie.find(inputWord)!=null){
             return inputWord;
         }
 
-        //----Deletion Algorithm Below----
-        findDeletionDistance(inputWord, dictSuggest);
-        //----Insertion Distance Below----
-        findInsertionDistance(inputWord, dictSuggest);
-        //----Alteration Distance Below----
-        findAlterationDistance(inputWord, dictSuggest);
+        // ---- Deletion Algorithm Below ----
+        findDeletionDistance(inputWord, editDistOneDict, false, editDistTwoDict);
+        // ---- Insertion Distance Below ----
+        findInsertionDistance(inputWord, editDistOneDict, false, editDistTwoDict);
+        // ---- Alteration Distance Below ----
+        findAlterationDistance(inputWord, editDistOneDict, false, editDistTwoDict);
+        // ---- Transposition Distance Below ----
+        findTranspositionDistance(inputWord, editDistOneDict, false, editDistTwoDict);
 
 
-        // Iterate over the hashmap and get value the word
-        if (!dictSuggest.isEmpty()) {
-            String keyWithMaxValue = null;
-            int maxValue = Integer.MIN_VALUE;
 
-            // Iterate through the entries to find the key with the greatest value
-            for (Map.Entry<String, Integer> entry : dictSuggest.entrySet()) {
-                String key = entry.getKey();
-                int value = entry.getValue();
+        String bestWord = getBestWord(editDistOneDict);
+        if(bestWord != null){return bestWord;}
 
-                if (value > maxValue) {
-                    maxValue = value;
-                    keyWithMaxValue = key;
-                }
-            }
-            return keyWithMaxValue;
-        }
+        bestWord = getBestWord(editDistTwoDict);
+        if(bestWord != null){return bestWord;}
 
         return null;
     }
 
+    //Function that chooses the best word
+    private String getBestWord(HashMap<String, Integer> dict) {
+        if (!dict.isEmpty()) {
+            String keyWithMaxValue = null;
+            int maxValue = Integer.MIN_VALUE;
+            String firstAlphabeticalWord = null;
+
+            for (Map.Entry<String, Integer> entry : dict.entrySet()) {
+                String key = entry.getKey();
+                int value = entry.getValue();
+
+                // Check if this word has a higher frequency
+                if (value > maxValue) {
+                    maxValue = value;
+                    keyWithMaxValue = key;
+                    firstAlphabeticalWord = key;
+                }
+                // If the frequency is the same, choose the first word alphabetically
+                else if (value == maxValue && key.compareTo(firstAlphabeticalWord) < 0) {
+                    keyWithMaxValue = key;
+                    firstAlphabeticalWord = key;
+                }
+            }
+
+            return keyWithMaxValue;
+        }
+        return null;
+    }
+
+
+
     //Deletion Dist Helper Function
-    private void findDeletionDistance(String inputWord, HashMap<String, Integer> dict) {
+    private void findDeletionDistance(String inputWord, HashMap<String, Integer> editOneDict, boolean secondCheck, HashMap<String, Integer> editTwoDict) {
+        //If we are checking first edit distance then we need to hold the possible other vals
+        List<String> secondDistance = new ArrayList<String>();
+        // Boolean that holds if edit dist of 1 worked
+        boolean hasEditOne = false;
         for (int i = 0; i < inputWord.length(); i++) {
             String possibleWord = inputWord.substring(0, i) + inputWord.substring(i + 1);
-
             // Check if the possible word exists in the dictionary
             if (dictTrie.find(possibleWord) != null) {
-                dict.put(possibleWord, dictTrie.find(possibleWord).getValue());
+                editOneDict.put(possibleWord, dictTrie.find(possibleWord).getValue());
+                hasEditOne = true;
+            } else if (!secondCheck && !hasEditOne) {
+                secondDistance.add(possibleWord);
+            }
+        }
+        if(!hasEditOne && !secondCheck){
+            for(String word: secondDistance){
+                findInsertionDistance(word, editTwoDict, true, editTwoDict);
+                findDeletionDistance(word, editTwoDict, true, editTwoDict);
+                findAlterationDistance(word, editTwoDict, true, editTwoDict);
+                findTranspositionDistance(word, editTwoDict, true, editTwoDict);
             }
         }
     }
     //Insertion Distance Helper Function
-    private void findInsertionDistance(String inputWord, HashMap<String, Integer> dict) {
+    private void findInsertionDistance(String inputWord, HashMap<String, Integer> editOneDict, boolean secondCheck, HashMap<String, Integer> editTwoDict) {
+        //If we are checking first edit distance then we need to hold the possible other vals
+        List<String> secondDistance = new ArrayList<String>();
+        // Boolean that holds if edit dist of 1 worked
+        boolean hasEditOne = false;
         for (int i = 0; i <= inputWord.length(); i++) {
             for (char c = 'a'; c <= 'z'; c++) {
                 String possibleWord = inputWord.substring(0, i) + c + inputWord.substring(i);
-
                 // Check if the possible word exists in the dictionary
                 if (dictTrie.find(possibleWord) != null) {
-                    dict.put(possibleWord, dictTrie.find(possibleWord).getValue());
+                    editOneDict.put(possibleWord, dictTrie.find(possibleWord).getValue());
+                    //Since there is a word with edit dist of one, make it true
+                    hasEditOne = true;
+                //This will add the words to another list that will be used for edit dist 2
+                } else if (!secondCheck && !hasEditOne) {
+                    secondDistance.add(possibleWord);
                 }
             }
         }
+        if(!hasEditOne && !secondCheck){
+            for(String word: secondDistance){
+                findInsertionDistance(word, editTwoDict, true, editTwoDict);
+                findDeletionDistance(word, editTwoDict, true, editTwoDict);
+                findAlterationDistance(word, editTwoDict, true, editTwoDict);
+                findTranspositionDistance(word, editTwoDict, true, editTwoDict);
+            }
+        }
+
     }
-    //Alteration Distance Function TODO: This needs to be fixed
-    private void findAlterationDistance(String inputWord, HashMap<String, Integer> dict) {
+    //Alteration Distance Function
+    private void findAlterationDistance(String inputWord, HashMap<String, Integer> editOneDict, boolean secondCheck, HashMap<String, Integer> editTwoDict) {
+        //If we are checking first edit distance then we need to hold the possible other vals
+        List<String> secondDistance = new ArrayList<String>();
+        // Boolean that holds if edit dist of 1 worked
+        boolean hasEditOne = false;
         for (int i = 0; i < inputWord.length(); i++) {
             for (char c = 'a'; c <= 'z'; c++) {
                 if (c != inputWord.charAt(i)) {
                     String possibleWord = inputWord.substring(0, i) + c + inputWord.substring(i + 1);
-
                     // Check if the possible word exists in the dictionary
                     if (dictTrie.find(possibleWord) != null) {
-                        dict.put(possibleWord, dictTrie.find(possibleWord).getValue());
+                        editOneDict.put(possibleWord, dictTrie.find(possibleWord).getValue());
+                        hasEditOne = true;
+                    } else if (!secondCheck && !hasEditOne) {
+                        secondDistance.add(possibleWord);
                     }
                 }
             }
         }
+        if(!hasEditOne && !secondCheck){
+            for(String word: secondDistance){
+                findInsertionDistance(word, editTwoDict, true, editTwoDict);
+                findDeletionDistance(word, editTwoDict, true, editTwoDict);
+                findAlterationDistance(word, editTwoDict, true, editTwoDict);
+                findTranspositionDistance(word, editTwoDict, true, editTwoDict);
+            }
+        }
     }
+    //Transposition Distance Function
+    private void findTranspositionDistance(String inputWord, HashMap<String, Integer> editOneDict,  boolean secondCheck, HashMap<String, Integer> editTwoDict) {
+        //If we are checking first edit distance then we need to hold the possible other vals
+        List<String> secondDistance = new ArrayList<String>();
+        // Boolean that holds if edit dist of 1 worked
+        boolean hasEditOne = false;
+        for (int i = 0; i < inputWord.length() - 1; i++) {
+            // Swap adjacent characters
+            char[] charArray = inputWord.toCharArray();
+            char temp = charArray[i];
+            charArray[i] = charArray[i + 1];
+            charArray[i + 1] = temp;
+            String possibleWord = new String(charArray);
+
+            // Check if the possible word exists in the dictionary
+            if (dictTrie.find(possibleWord) != null) {
+                editOneDict.put(possibleWord, dictTrie.find(possibleWord).getValue());
+                hasEditOne = true;
+            } else if (!secondCheck && !hasEditOne) {
+                secondDistance.add(possibleWord);
+            }
+        }
+        if(!hasEditOne && !secondCheck){
+            for(String word: secondDistance){
+                findInsertionDistance(word, editTwoDict, true, editTwoDict);
+                findDeletionDistance(word, editTwoDict, true, editTwoDict);
+                findAlterationDistance(word, editTwoDict, true, editTwoDict);
+                findTranspositionDistance(word, editTwoDict, true, editTwoDict);
+            }
+        }
+    }
+
 
 
 }
